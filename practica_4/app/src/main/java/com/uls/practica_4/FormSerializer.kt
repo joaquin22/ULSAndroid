e0c7adf3ea3e88586425e8128537f90168ba1abe
Joaquin <quinino1@gmail.com>
2026-04-15 21:41:50 -0500

    Add form input fields and serialization for user data in MainActivity

---
+++ practica_4/app/src/main/java/com/uls/practica_4/FormSerializer.kt
@@ -0,0 +1,8 @@
+package com.uls.practica_4
+
+object FormSerializer {
+    const val NAME = "name"
+    const val AGE = "age"
+    const val CITY = "city"
+    const val EMAIL = "email"
+}
--- practica_4/app/src/main/java/com/uls/practica_4/MainActivity.kt
+++ practica_4/app/src/main/java/com/uls/practica_4/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.uls.practica_4
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnSubmit = findViewById<Button>(R.id.btn_send_form)
+        btnSubmit.setOnClickListener { onSubmit() }
+    }
+
+    fun onSubmit() {
+        val name = findViewById<TextInputLayout>(R.id.text_input_name).editText!!.text.toString()
+        val age = findViewById<TextInputLayout>(R.id.text_input_age).editText!!.text.toString()
+        val city = findViewById<TextInputLayout>(R.id.text_input_city).editText!!.text.toString()
+        val email = findViewById<TextInputLayout>(R.id.text_input_email).editText!!.text.toString()
+
+        val intent = Intent(this, ResumeActivity::class.java)
+        Log.d("MainActivity", "Creating intent to start ResumeActivity with data: name=$name, age=$age, city=$city, email=$email")
+        intent.putExtra(FormSerializer.NAME, name)
+        intent.putExtra(FormSerializer.AGE, age)
+        intent.putExtra(FormSerializer.CITY, city)
+        intent.putExtra(FormSerializer.EMAIL, email)
+
+        startActivity(intent)
     }
 }
