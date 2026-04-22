a128cf3a9ed830ca6c7ec55860beec7b83c9bd30
Joaquin <quinino1@gmail.com>
2026-04-21 20:38:17 -0500

    Rename FormSerializer to UserSerializer and change constants to val

--- practica_4/app/src/main/java/com/uls/practica_4/FormSerializer.kt
+++ practica_4/app/src/main/java/com/uls/practica_4/FormSerializer.kt
@@ -1,8 +1,8 @@
 package com.uls.practica_4
 
-object FormSerializer {
-    const val NAME = "name"
-    const val AGE = "age"
-    const val CITY = "city"
-    const val EMAIL = "email"
+object UserSerializer {
+     val NAME = "name"
+     val AGE = "age"
+     val EMAIL = "email"
+     val CITY = "city"
 }
