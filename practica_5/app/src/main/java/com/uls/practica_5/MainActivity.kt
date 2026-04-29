99fb61eff0ebe3d862bbecb3138512eccbbb0192
Joaquin <quinino1@gmail.com>
2026-04-29 10:03:06 -0500

    Replace Greeting composable with FormScreen in MainActivity

--- practica_5/app/src/main/java/com/uls/practica_5/MainActivity.kt
+++ practica_5/app/src/main/java/com/uls/practica_5/MainActivity.kt
@@ -7,10 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.uls.practica_5.ui.screens.FormScreen
 import com.uls.practica_5.ui.theme.Practica_5Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +18,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Practica_5Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                FormScreen(
+                    modifier = Modifier.padding(innerPadding)
+                )
             }
+
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Practica_5Theme {
-        Greeting("Android")
+        FormScreen()
     }
 }
