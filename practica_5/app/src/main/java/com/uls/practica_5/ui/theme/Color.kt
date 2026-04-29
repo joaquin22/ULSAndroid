879ea771bbe4895e2b37eb2bd04eefcac0708c2f
Joaquin <quinino1@gmail.com>
2026-04-29 10:02:46 -0500

    Add new color definitions for Slate and Error

--- practica_5/app/src/main/java/com/uls/practica_5/ui/theme/Color.kt
+++ practica_5/app/src/main/java/com/uls/practica_5/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Slate = Color(0xFF4c5679)
+val Error = Color(0xFFE84639)
